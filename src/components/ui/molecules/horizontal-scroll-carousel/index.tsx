ea6d30a567e7ec3ba1ce940ce16055fd47583899
letelete <brunokawka@gmail.com>
2024-03-12 20:57:51 +0100

    Add responsiveness to the `HorizontalScrollCarousel`

--- src/components/ui/molecules/horizontal-scroll-carousel/index.tsx
+++ src/components/ui/molecules/horizontal-scroll-carousel/index.tsx
@@ -1,5 +1,3 @@
-;
-
 import { motion, useScroll, useTransform } from 'framer-motion';
 import Link from 'next/link';
 import {
@@ -38,7 +36,7 @@ const HorizontalScrollCarousel = forwardRef<
       className,
       reversed,
       childrenContainerClass,
-      scrollHeightFactor = 2,
+      scrollHeightFactor = 3,
       ...rest
     },
     handleRef
@@ -49,13 +47,30 @@ const HorizontalScrollCarousel = forwardRef<
     const childrenContainerRef = useRef<HTMLDivElement>(null);
     const childrenContainerGeometry = useElementGeometry(childrenContainerRef);
     const childrenContainerWidth = childrenContainerGeometry?.width ?? 0;
+
+    const targetGeometry = useElementGeometry(targetRef);
     const targetHeight = childrenContainerWidth * scrollHeightFactor;
+    const targetWidth = targetGeometry?.width ?? 0;
+
+    /**
+     * Width provided by the `layout-width-limitter`.
+     */
+    const layoutWidth = Math.min(targetWidth, 1024);
+    const layoutStartPositionX = (targetWidth - layoutWidth) / 2;
 
     const { scrollYProgress } = useScroll({
       target: targetRef,
     });
 
-    const outputRange = reversed ? ['15%', '-75%'] : ['-75%', '15%'];
+    const outputRange = reversed
+      ? [
+          `${layoutStartPositionX + layoutWidth * 0.33}px`,
+          `${layoutStartPositionX - childrenContainerWidth + layoutWidth - 16}px`,
+        ]
+      : [
+          `${layoutStartPositionX - childrenContainerWidth + layoutWidth * 0.66}px`,
+          `${layoutStartPositionX + 16}px`,
+        ];
     const x = useTransform(scrollYProgress, [0, 1], outputRange);
 
     return (
@@ -118,11 +133,11 @@ const HorizontalScrollButtonContainer = forwardRef<
     <motion.div
       className='relative z-0 my-auto p-8'
       whileInView={{ opacity: 1, x: 0 }}
-      initial={{ opacity: 0, x: reversed ? -100 : 100 }}
+      initial={{ opacity: 0, x: reversed ? -16 : 16 }}
       transition={{ type: 'spring', bounce: 0 }}
       viewport={{
         root: horizontalScrollCarouselRef as RefObject<HTMLDivElement>,
-        amount: 'all',
+        amount: 0.8,
         once: true,
       }}
     >
