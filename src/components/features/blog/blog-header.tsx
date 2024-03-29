427131fd8ceac2b8310a99978f879c0368ccf28d
letelete <brunokawka@gmail.com>
2024-03-29 16:11:09 +0100

    Restore normal app-header size with offset
    This eliminates an issue with AppBar animating when user wants to scroll back just a bit (e.g to see a previous paragraph).

--- src/components/features/blog/blog-header.tsx
+++ src/components/features/blog/blog-header.tsx
@@ -1,11 +1,22 @@
 'use client';
 
+import { Variants, motion } from 'framer-motion';
 import Link from 'next/link';
 import { forwardRef } from 'react';
 
 import { UnicornEmoji } from '~ui/atoms/emojis';
 import { Typography } from '~ui/atoms/typography';
 import { AppHeader, AppHeaderProps } from '~ui/molecules/app-header';
+
+const itemMotionVariants: Variants = {
+  compact: { scale: 0.875 },
+  normal: { scale: 1 },
+} as const;
+
+const itemWithColorsMotionVariants: Variants = {
+  compact: { ...itemMotionVariants.compact, filter: 'grayscale(1)' },
+  normal: { ...itemMotionVariants.normal, filter: 'grayscale(0)' },
+} as const;
 
 const BlogHeader = forwardRef<HTMLDivElement, AppHeaderProps>((props, ref) => {
   return (
@@ -16,16 +27,20 @@ const BlogHeader = forwardRef<HTMLDivElement, AppHeaderProps>((props, ref) => {
       ref={ref}
     >
       <Link href='/blog'>
-        <Typography variant='body' color='highlight'>
-          <span className='font-bold'> blog</span>
-          .kawka.me
+        <Typography variant='body' color='highlight' asChild>
+          <motion.p variants={itemMotionVariants}>
+            <span className='font-bold'> blog</span>
+            .kawka.me
+          </motion.p>
         </Typography>
       </Link>
 
       <Link href='/'>
-        <Typography variant='body' color='highlight'>
-          <UnicornEmoji className='mr-2' />
-          about me
+        <Typography variant='body' color='highlight' asChild>
+          <motion.p variants={itemWithColorsMotionVariants}>
+            <UnicornEmoji className='mr-2' />
+            about me
+          </motion.p>
         </Typography>
       </Link>
     </AppHeader>
--- src/components/ui/molecules/app-header/index.tsx
+++ src/components/ui/molecules/app-header/index.tsx
@@ -1,10 +1,14 @@
 import {
   HTMLMotionProps,
   motion,
+  useMotionValue,
   useMotionValueEvent,
   useScroll,
+  useVelocity,
 } from 'framer-motion';
 import { ReactNode, forwardRef, useState } from 'react';
+
+import useTailwind from '~hooks/use-tailwind';
 
 import { cn, vhToPx } from '~utils/style';
 
@@ -15,28 +19,69 @@ export interface AppHeaderProps extends HTMLMotionProps<'header'> {
 
 const AppHeader = forwardRef<HTMLDivElement, AppHeaderProps>(
   ({ className, innerClassName, children, ...rest }, ref) => {
-    const { scrollY } = useScroll();
+    const tw = useTailwind();
+
     const [compact, setCompact] = useState(false);
 
-    useMotionValueEvent(scrollY, 'change', (latest) => {
-      const previous = scrollY.getPrevious();
-      const delta = latest - (previous ?? 0);
-      const isCompactView = delta > 0 && latest > vhToPx(30);
+    const { scrollY } = useScroll();
+    const scrollVelocity = useVelocity(scrollY);
+    const scrollDistanceStartY = useMotionValue(0);
 
-      setCompact(isCompactView);
+    useMotionValueEvent(scrollVelocity, 'change', (latest) => {
+      // If scroll is slow enough, the velocity will reset `scrollDistanceStartY`
+      // to avoid displaying normal view immediately when user scrolls up.
+      if (latest * 10000 === 0) {
+        scrollDistanceStartY.set(scrollY.get());
+      }
+    });
+
+    useMotionValueEvent(scrollY, 'change', (latest) => {
+      const MIN_SCROLL_OFFSET_COMPACT_VIEW = 0;
+      const MIN_SCROLL_OFFSET_NORMAL_VIEW = 512;
+
+      const previous = scrollY.getPrevious() ?? 0;
+      const delta = latest - previous;
+      const forceNormalView = latest <= vhToPx(30);
+
+      let distance = latest - scrollDistanceStartY.get();
+
+      if (forceNormalView) {
+        setCompact(false);
+      } else {
+        const directionChanged = delta * distance < 0;
+        if (directionChanged) {
+          scrollDistanceStartY.set(latest);
+          distance = 0;
+        }
+
+        const scrollOffset = Math.abs(distance);
+        const isScrollingDown = delta > 0;
+
+        setCompact((isNowCompact) =>
+          isScrollingDown
+            ? scrollOffset >= MIN_SCROLL_OFFSET_COMPACT_VIEW
+            : isNowCompact && scrollOffset < MIN_SCROLL_OFFSET_NORMAL_VIEW
+        );
+      }
     });
 
     return (
       <motion.header
         ref={ref}
         variants={{
-          compact: { padding: '0.5rem 0' },
-          normal: { padding: '2.5rem 0' },
+          compact: {
+            padding: '0.5rem 0',
+            borderColor: tw.theme.colors.background.contrast,
+          },
+          normal: {
+            padding: '2.5rem 0',
+            borderColor: 'transparent',
+          },
         }}
         initial='normal'
         animate={compact ? 'compact' : 'normal'}
         className={cn(
-          'sticky top-0 z-50 flex w-full bg-gradient-to-b from-background to-background/50 backdrop-blur-md',
+          'sticky top-0 z-50 flex w-full border-b bg-gradient-to-b from-background to-background/50 backdrop-blur-md',
           className
         )}
         {...rest}
