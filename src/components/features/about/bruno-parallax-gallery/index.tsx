b4a94a57c67711f77e2628e552c0baad011d9cf9
letelete <brunokawka@gmail.com>
2024-04-11 11:06:41 +0200

    Add new image to about me

--- src/components/features/about/bruno-parallax-gallery/index.tsx
+++ src/components/features/about/bruno-parallax-gallery/index.tsx
@@ -1,20 +1,28 @@
 import { GalleryItem, ParallaxGallery } from '~ui/organisms/parallax-gallery';
 
-import img0 from '/public/galleries/about-me/img-0.webp';
-import img10 from '/public/galleries/about-me/img-10.webp';
-import img20 from '/public/galleries/about-me/img-20.webp';
-import img30 from '/public/galleries/about-me/img-30.webp';
-import img40 from '/public/galleries/about-me/img-40.webp';
-import img50 from '/public/galleries/about-me/img-50.webp';
-import img60 from '/public/galleries/about-me/img-60.webp';
+import img0050 from '/public/galleries/about-me/0050.webp';
+import img0100 from '/public/galleries/about-me/0100.webp';
+import img0200 from '/public/galleries/about-me/0200.webp';
+import img0300 from '/public/galleries/about-me/0300.webp';
+import img0400 from '/public/galleries/about-me/0400.webp';
+import img0500 from '/public/galleries/about-me/0500.webp';
+import img0600 from '/public/galleries/about-me/0600.webp';
+import img0700 from '/public/galleries/about-me/0700.webp';
 
 // TODO: research better ways of preloading images https://github.com/vercel/next.js/discussions/34644
-const items = [img0, img20, img30, img40, img50, img60, img10].map<GalleryItem>(
-  (src) => ({
-    alt: 'Bruno Kawka',
-    src,
-  })
-);
+const items = [
+  img0050,
+  img0100,
+  img0200,
+  img0300,
+  img0400,
+  img0500,
+  img0600,
+  img0700,
+].map<GalleryItem>((src) => ({
+  alt: 'Bruno Kawka',
+  src,
+}));
 
 export const BrunoParallaxGallery = () => {
   return <ParallaxGallery className='object-cover' items={items} />;
